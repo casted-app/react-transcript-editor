97d6be51f1c3bcb1aedb87e40613d2858e289ba5
Josh <josh@casted.us>
2022-12-15 16:42:28 -0500

    update packages to use latest and for github packages

--- packages/components/timed-text-editor/WrapperBlock.js
+++ packages/components/timed-text-editor/WrapperBlock.js
@@ -1,218 +1,247 @@
 import React from 'react';
-import { 
-  EditorBlock, 
-  Modifier, 
-  EditorState, 
-  SelectionState,
-  convertFromRaw,
-  convertToRaw
- } from 'draft-js';
- import VisibilitySensor from 'react-visibility-sensor';
+import {
+	EditorBlock,
+	Modifier,
+	EditorState,
+	SelectionState,
+	convertFromRaw,
+	convertToRaw,
+} from '@casted-app/draft-js';
+import VisibilitySensor from 'react-visibility-sensor';
 import SpeakerLabel from './SpeakerLabel';
 // import { shortTimecode, secondsToTimecode } from '../../Util/timecode-converter/';
 
 import {
-  shortTimecode,
-  secondsToTimecode
+	shortTimecode,
+	secondsToTimecode,
 } from '../../util/timecode-converter';
 
 import style from './WrapperBlock.module.css';
 
 const updateSpeakerName = (oldName, newName, state) => {
-  const contentToUpdate = convertToRaw(state);
+	const contentToUpdate = convertToRaw(state);
 
-  contentToUpdate.blocks.forEach(block => {
-    if (block.data.speaker === oldName) {
-      block.data.speaker = newName;
-    }
-  })
+	contentToUpdate.blocks.forEach((block) => {
+		if (block.data.speaker === oldName) {
+			block.data.speaker = newName;
+		}
+	});
 
-  return convertFromRaw(contentToUpdate);
-}
-
+	return convertFromRaw(contentToUpdate);
+};
 
 class WrapperBlock extends React.Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      speaker: '',
-      start: 0,
-      timecodeOffset: this.props.blockProps.timecodeOffset
-    };
-  }
+		this.state = {
+			speaker: '',
+			start: 0,
+			timecodeOffset: this.props.blockProps.timecodeOffset,
+		};
+	}
 
-  componentDidMount() {
-    const { block } = this.props;
-    const speaker = block.getData().get('speaker');
-  
-    const start = block.getData().get('start');
+	componentDidMount() {
+		const { block } = this.props;
+		const speaker = block.getData().get('speaker');
 
-    this.setState({
-      speaker: speaker,
-      start: start,
-      getElement: document.querySelector(".notranslate")
-    });
-  }
-  // reducing unnecessary re-renders
-  shouldComponentUpdate = (nextProps, nextState) => {
-    if (nextProps.block.getText() !== this.props.block.getText()) {
-      return true;
-    }
+		const start = block.getData().get('start');
 
-    if (nextProps.blockProps.showSpeakers !== this.props.blockProps.showSpeakers) {
-      return true;
-    }
+		this.setState({
+			speaker: speaker,
+			start: start,
+			getElement: document.querySelector('.notranslate'),
+		});
+	}
+	// reducing unnecessary re-renders
+	shouldComponentUpdate = (nextProps, nextState) => {
+		if (nextProps.block.getText() !== this.props.block.getText()) {
+			return true;
+		}
 
-    if (nextProps.blockProps.showTimecodes !== this.props.blockProps.showTimecodes) {
-      return true;
-    }
+		if (
+			nextProps.blockProps.showSpeakers !== this.props.blockProps.showSpeakers
+		) {
+			return true;
+		}
 
-    if (nextProps.blockProps.timecodeOffset !== this.props.blockProps.timecodeOffset) {
-      return true;
-    }
+		if (
+			nextProps.blockProps.showTimecodes !== this.props.blockProps.showTimecodes
+		) {
+			return true;
+		}
 
-    if (nextState.speaker !== this.state.speaker) {
-      return true;
-    }
+		if (
+			nextProps.blockProps.timecodeOffset !==
+			this.props.blockProps.timecodeOffset
+		) {
+			return true;
+		}
 
-    if (nextProps.blockProps.isEditable !== this.props.blockProps.isEditable) {
-      return true;
-    }
+		if (nextState.speaker !== this.state.speaker) {
+			return true;
+		}
 
-    if(nextProps.block.getData().get('speaker') !== this.state.speaker){
-      return true;
-    }
-    return false;
-  };
+		if (nextProps.blockProps.isEditable !== this.props.blockProps.isEditable) {
+			return true;
+		}
 
-  componentDidUpdate  = (prevProps, prevState) =>{
+		if (nextProps.block.getData().get('speaker') !== this.state.speaker) {
+			return true;
+		}
+		return false;
+	};
 
-    if(prevProps.block.getData().get('speaker') !== prevState.speaker){
-        
-        this.setState({
-          speaker: prevProps.block.getData().get('speaker')
-        })
+	componentDidUpdate = (prevProps, prevState) => {
+		if (prevProps.block.getData().get('speaker') !== prevState.speaker) {
+			this.setState({
+				speaker: prevProps.block.getData().get('speaker'),
+			});
 
-        return true;
-      }
-  }
+			return true;
+		}
+	};
 
-  handleOnClickEdit = () => {
-    const oldSpeakerName = this.state.speaker;
-    const newSpeakerName = prompt('New Speaker Name?', this.state.speaker);
-    if (newSpeakerName !== '' && newSpeakerName !== null) {
-      this.setState({ speaker: newSpeakerName });
-      const isUpdateAllSpeakerInstances = confirm(`Would you like to replace all occurrences of ${oldSpeakerName} with ${newSpeakerName}?`);
-     
-      if (this.props.blockProps.handleAnalyticsEvents) {
-        this.props.blockProps.handleAnalyticsEvents({
-          category: 'WrapperBlock',
-          action: 'handleOnClickEdit',
-          name: 'newSpeakerName',
-          value: newSpeakerName
-        });
-      }
+	handleOnClickEdit = () => {
+		const oldSpeakerName = this.state.speaker;
+		const newSpeakerName = prompt('New Speaker Name?', this.state.speaker);
+		if (newSpeakerName !== '' && newSpeakerName !== null) {
+			this.setState({ speaker: newSpeakerName });
+			const isUpdateAllSpeakerInstances = confirm(
+				`Would you like to replace all occurrences of ${oldSpeakerName} with ${newSpeakerName}?`
+			);
 
-      if(isUpdateAllSpeakerInstances){
-        const ContentState = this.props.blockProps.editorState.getCurrentContent();
-        const contentToUpdateWithSpekaers = updateSpeakerName(oldSpeakerName, newSpeakerName, ContentState);
-        this.props.blockProps.setEditorNewContentStateSpeakersUpdate(contentToUpdateWithSpekaers);
-      }
-      else{
-       // From docs: https://draftjs.org/docs/api-reference-selection-state#keys-and-offsets
-        // selection points are tracked as key/offset pairs,
-        // where the key value is the key of the ContentBlock where the point is positioned
-        // and the offset value is the character offset within the block.
+			if (this.props.blockProps.handleAnalyticsEvents) {
+				this.props.blockProps.handleAnalyticsEvents({
+					category: 'WrapperBlock',
+					action: 'handleOnClickEdit',
+					name: 'newSpeakerName',
+					value: newSpeakerName,
+				});
+			}
 
-        // Get key of the currentBlock
-        const keyForCurrentCurrentBlock = this.props.block.getKey();
-        // create empty selection for current block
-        // https://draftjs.org/docs/api-reference-selection-state#createempty
-        const currentBlockSelection = SelectionState.createEmpty(
-          keyForCurrentCurrentBlock
-        );
-        const editorStateWithSelectedCurrentBlock = EditorState.acceptSelection(
-          this.props.blockProps.editorState,
-          currentBlockSelection
-        );
+			if (isUpdateAllSpeakerInstances) {
+				const ContentState =
+					this.props.blockProps.editorState.getCurrentContent();
+				const contentToUpdateWithSpekaers = updateSpeakerName(
+					oldSpeakerName,
+					newSpeakerName,
+					ContentState
+				);
+				this.props.blockProps.setEditorNewContentStateSpeakersUpdate(
+					contentToUpdateWithSpekaers
+				);
+			} else {
+				// From docs: https://draftjs.org/docs/api-reference-selection-state#keys-and-offsets
+				// selection points are tracked as key/offset pairs,
+				// where the key value is the key of the ContentBlock where the point is positioned
+				// and the offset value is the character offset within the block.
 
-        const currentBlockSelectionState = editorStateWithSelectedCurrentBlock.getSelection();
-        const newBlockDataWithSpeakerName = { speaker: newSpeakerName };
+				// Get key of the currentBlock
+				const keyForCurrentCurrentBlock = this.props.block.getKey();
+				// create empty selection for current block
+				// https://draftjs.org/docs/api-reference-selection-state#createempty
+				const currentBlockSelection = SelectionState.createEmpty(
+					keyForCurrentCurrentBlock
+				);
+				const editorStateWithSelectedCurrentBlock = EditorState.acceptSelection(
+					this.props.blockProps.editorState,
+					currentBlockSelection
+				);
 
-        // https://draftjs.org/docs/api-reference-modifier#mergeblockdata
-        const newContentState = Modifier.mergeBlockData(
-          this.props.contentState,
-          currentBlockSelectionState,
-          newBlockDataWithSpeakerName
-        );
+				const currentBlockSelectionState =
+					editorStateWithSelectedCurrentBlock.getSelection();
+				const newBlockDataWithSpeakerName = { speaker: newSpeakerName };
 
-        this.props.blockProps.setEditorNewContentStateSpeakersUpdate(newContentState);
-      }
-    }
-  };
+				// https://draftjs.org/docs/api-reference-modifier#mergeblockdata
+				const newContentState = Modifier.mergeBlockData(
+					this.props.contentState,
+					currentBlockSelectionState,
+					newBlockDataWithSpeakerName
+				);
 
-  handleTimecodeClick = () => {
-    this.props.blockProps.onWordClick(this.state.start);
-    if (this.props.blockProps.handleAnalyticsEvents) {
-      this.props.blockProps.handleAnalyticsEvents({
-        category: 'WrapperBlock',
-        action: 'handleTimecodeClick',
-        name: 'onWordClick',
-        value: secondsToTimecode(this.state.start)
-      });
-    }
-  };
+				this.props.blockProps.setEditorNewContentStateSpeakersUpdate(
+					newContentState
+				);
+			}
+		}
+	};
 
-  visibilityChanged = (isVisible) => {
-    // console.log("ISVISIBLE", isVisible);
-  }
+	handleTimecodeClick = () => {
+		this.props.blockProps.onWordClick(this.state.start);
+		if (this.props.blockProps.handleAnalyticsEvents) {
+			this.props.blockProps.handleAnalyticsEvents({
+				category: 'WrapperBlock',
+				action: 'handleTimecodeClick',
+				name: 'onWordClick',
+				value: secondsToTimecode(this.state.start),
+			});
+		}
+	};
 
-  render() {
+	visibilityChanged = (isVisible) => {
+		// console.log("ISVISIBLE", isVisible);
+	};
 
-    let containmentDOMRect = this.state.getElement
-      ? this.state.getElement
-      : null;
+	render() {
+		let containmentDOMRect = this.state.getElement
+			? this.state.getElement
+			: null;
 
-    let startTimecode = this.state.start;
-    if (this.props.blockProps.timecodeOffset) {
-      startTimecode += this.props.blockProps.timecodeOffset;
-    }
+		let startTimecode = this.state.start;
+		if (this.props.blockProps.timecodeOffset) {
+			startTimecode += this.props.blockProps.timecodeOffset;
+		}
 
-    const speakerElement = (
-      <SpeakerLabel
-        name={ this.state.speaker }
-        handleOnClickEdit={ this.handleOnClickEdit }
-        isEditable={this.props.blockProps.isEditable}
-      />
-    );
+		const speakerElement = (
+			<SpeakerLabel
+				name={this.state.speaker}
+				handleOnClickEdit={this.handleOnClickEdit}
+				isEditable={this.props.blockProps.isEditable}
+			/>
+		);
 
-    const timecodeElement = (
-      <span className={ style.time } onClick={ this.handleTimecodeClick }>
-        {shortTimecode(startTimecode)}
-      </span>
-    );
+		const timecodeElement = (
+			<span className={style.time} onClick={this.handleTimecodeClick}>
+				{shortTimecode(startTimecode)}
+			</span>
+		);
 
-    return (
-      <VisibilitySensor partialVisibility={true} onChange={this.visibilityChanged} containment={containmentDOMRect} scrollCheck>
-         {({isVisible}) =>  isVisible ?
-          <div className={ style.WrapperBlock }>
-              <div
-                className={ [ style.markers, style.unselectable ].join(' ') }
-                contentEditable={ false }
-              >
-                {this.props.blockProps.showSpeakers ? speakerElement : ''}
+		return (
+			<VisibilitySensor
+				partialVisibility={true}
+				onChange={this.visibilityChanged}
+				containment={containmentDOMRect}
+				scrollCheck
+			>
+				{({ isVisible }) =>
+					isVisible ? (
+						<div className={style.WrapperBlock}>
+							<div
+								className={[style.markers, style.unselectable].join(' ')}
+								contentEditable={false}
+							>
+								{this.props.blockProps.showSpeakers ? speakerElement : ''}
 
-                {this.props.blockProps.showTimecodes ? timecodeElement : ''}
-              </div>
-              <div className={ style.text }>
-                <EditorBlock { ...this.props } />
-              </div>
-            </div> : <div className={ style.WrapperBlock + " " + style.loadingBlockPlaceholder }>loading...</div>
-          }
-        </VisibilitySensor>
-    );
-  }
+								{this.props.blockProps.showTimecodes ? timecodeElement : ''}
+							</div>
+							<div className={style.text}>
+								<EditorBlock {...this.props} />
+							</div>
+						</div>
+					) : (
+						<div
+							className={
+								style.WrapperBlock + ' ' + style.loadingBlockPlaceholder
+							}
+						>
+							loading...
+						</div>
+					)
+				}
+			</VisibilitySensor>
+		);
+	}
 }
 
 export default WrapperBlock;
